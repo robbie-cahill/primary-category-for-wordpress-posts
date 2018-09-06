774a72828c82798fcf20ce721c071fdd457825c4
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-09-06 13:52:02 +1000

    Added a comment about usability

--- php/class-primary-category.php
+++ php/class-primary-category.php
@@ -34,6 +34,10 @@ class Primary_Category {
 	 * For smart people who know how to pull of a CSRF attack, this will give you a nice read only list of categories that are going to be public anyway on the front end
 	 * Because the categories are public information, I chose not to add nonce verification here.
 	 * This makes Select2's live search alot faster as a new nonce would have to be generated on each keypress.
+	 *
+	 * For usability purposes, I'm allowing all categories to come back as results, not just categories that are selected for this post
+	 * This makes selecting a primary category a one step process
+	 * The user will only have to publish the post once after selecting a category and primary category, not select a category, then publish, select a primary category then publish again.
 	 */
 	public function admin_ajax_primary_category_query() : void {
 		$name = filter_input( INPUT_GET, 'term', FILTER_SANITIZE_STRING ); // Select2 sends "term" for the search query by default
