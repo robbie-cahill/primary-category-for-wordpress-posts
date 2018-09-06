02736aefb133432f56fc5e610051c7b6786be0ef
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-09-06 10:13:03 +1000

    Initial Commit

---
+++ php/class-primary-category.php
@@ -0,0 +1,27 @@
+<?php
+namespace TenUp\Primary_Category;
+
+class Primary_Category {
+
+	/**
+	 * @action admin_init
+	 */
+	public function admin_init() {
+
+	}
+
+	public function adding_custom_meta_boxes( $post ) {
+		add_meta_box(
+			'my-meta-box',
+			'Primary Category',
+			[ $this, 'render_primary_category_meta_box' ],
+			'post',
+			'normal',
+			'default'
+		);
+	}
+
+	public function render_primary_category_meta_box() {
+		echo 'TEST';
+	}
+}
---
+++ primary-category.php
@@ -0,0 +1,14 @@
+<?php
+namespace TenUp\Primary_Category;
+/*
+ * Plugin Name: Primary Category For Posts
+ * Description: Set a Primary Category for your posts. Whats that? WordPress doesn't come with Primary Cateogries? No worries!
+ * License: GPLv3+
+ * Text Domain: primary-category-for-posts
+ */
+
+require __DIR__ . '/php/class-primary-category.php';
+
+$primary_category = new Primary_Category();
+add_action( 'admin_init', [ $primary_category, 'admin_init' ] );
+add_action( 'add_meta_boxes_post', [ $primary_category, 'adding_custom_meta_boxes' ] );
