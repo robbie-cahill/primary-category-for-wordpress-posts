7b25df11da5bdb8fcebff812a0eb28faa7006709
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-09-06 13:00:19 +1000

    Strict types for all functions

--- php/class-primary-category.php
+++ php/class-primary-category.php
@@ -6,14 +6,14 @@ class Primary_Category {
 	/**
 	 * @action admin_enqueue_scripts
 	 */
-	public function admin_enqueue_scripts() {
+	public function admin_enqueue_scripts() : void {
 		wp_enqueue_style( 'select2-css', plugin_dir_url( __FILE__ ) . '../node_modules/select2/dist/css/select2.css' );
 		wp_enqueue_style( 'primary-category-for-posts-css', plugin_dir_url( __FILE__ ) . '../assets/css/primary-category.css' );
 		wp_enqueue_script( 'select2', plugin_dir_url( __FILE__ ) . '../node_modules/select2/dist/js/select2.full.js', [ 'jquery' ], '0.0.1', true );
 		wp_enqueue_script( 'primary-category-for-posts', plugin_dir_url( __FILE__ ) . '../assets/js/primary-category.js', [ 'select2', 'jquery' ], '0.0.1', true );
 	}
 
-	public function add_meta_box( $post ) {
+	public function add_meta_box( $post ) : void {
 		add_meta_box(
 			'primary-category-meta-box',
 			'Primary Category',
@@ -24,7 +24,7 @@ class Primary_Category {
 		);
 	}
 
-	public function admin_ajax_primary_category_query() {
+	public function admin_ajax_primary_category_query() : void {
 		$name = filter_input( INPUT_GET, 'term', FILTER_SANITIZE_STRING ); // Select2 sends "term" for the search query by default
 
 		/**
@@ -64,18 +64,18 @@ class Primary_Category {
 	/**
 	 * @action save_post
 	 */
-	public function process_primary_category() {
+	public function process_primary_category() : void {
 		$post_id = intval( filter_input( INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT ) );
 		$primary_category = intval( filter_input( INPUT_POST, 'primary-category', FILTER_SANITIZE_NUMBER_INT ) );
 
 		if ( 0 === $primary_category || 0 === $post_id ) {
-			return true;
+			return;
 		}
 
 		update_post_meta( $post_id, 'primary_category', $primary_category );
 	}
 
-	public function render_primary_category_meta_box( $post ) {
+	public function render_primary_category_meta_box( $post ) : void {
 		$primary_category = get_post_meta( $post->ID, 'primary_category', true );
 		$term_id = null;
 
